dfb79c8136bf8fe4e92bce848d7f14b8cc06ad19
Shubham Pampattiwar <shubhampampattiwar7@gmail.com>
2022-01-13 20:37:46 -0500

    OADP-200: Limit velero servicemonitor to keep bounded cardinality

--- controllers/monitor.go
+++ controllers/monitor.go
@@ -99,10 +99,17 @@ func (r *DPAReconciler) buildVeleroServiceMonitor(serviceMonitor *monitor.Servic
 		{
 			Interval: "30s",
 			Port:     "monitoring",
+			MetricRelabelConfigs: []*monitor.RelabelConfig{
+				{
+					Action: "keep",
+					Regex:  ("velero_backup_total|velero_restore_total"),
+					SourceLabels: []string{
+						"__name__",
+					},
+				},
+			},
 		},
 	}
-
-	//serviceMonitor.Spec.JobLabel = "app"
 
 	serviceMonitor.Spec.NamespaceSelector = monitor.NamespaceSelector{
 		MatchNames: []string{
